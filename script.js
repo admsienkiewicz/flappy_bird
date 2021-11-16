61c80c995e674517cb2ef35580d426d3ebb6827a
admsienkiewicz <adm.sienkiewicz@gmail.com>
2021-11-16 10:50:34 +0100

    positioning fix

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let hole = document.getElementById("hole");
 let topPipe = document.getElementById("top");
 let bottomPipe = document.getElementById("bottom");
 let bird = document.getElementById("bird");
-let startMessage = document.getElementById('start-message');
-let startBox = document.getElementById('start-box');
+let startMessage = document.getElementById("start-message");
+let startBox = document.getElementById("start-box");
 let scoreBox = document.getElementById("scorebox");
 let personalBests = document.getElementById("score-list");
 let jumping = 0;
@@ -15,138 +15,123 @@ let checkIfOver;
 let score = 0;
 let animationSpeed;
 
-
 let scoreList = [];
 let uniqueScores;
 
 function startGame() {
+  if (!started) {
+    scoreBox.textContent = "Score: 0";
+    animationSpeed = 2;
+    score = 0;
+    pipe.style.animation = "none";
+    pipe.style.left = "500px";
+    bird.style.top = "350px";
 
-    if (!started) {
-        scoreBox.textContent = "Score: 0";
-        animationSpeed = 2;
-        score = 0;
-        pipe.style.animation = "none"
-        pipe.style.left = "500px"
-        bird.style.top = "350px"
+    setTimeout(function () {
+      pipe.style.animation =
+        "pipe-move " + parseInt(animationSpeed) + "s infinite linear";
+      gravityInterval = setInterval(gravity, 10);
+      checkIfOver = setInterval(gameOver, 5);
+    }, 20);
+    startBox.style.visibility = "hidden";
+    over = false;
+    started = true;
+  }
+  if (over) {
+    scoreList.push(score);
+    scoreList.sort(function compareNumbers(a, b) {
+      return a - b;
+    });
+    scoreList.reverse();
+    uniqueScores = [...new Set(scoreList)];
 
+    personalBests.textContent = "";
 
-        setTimeout(function () {
-            pipe.style.animation = "pipe-move " + parseInt(animationSpeed) + "s infinite linear";
-            gravityInterval = setInterval(gravity, 10);
-            checkIfOver = setInterval(gameOver, 5);
-
-        }, 20)
-        startBox.style.visibility = "hidden";
-        over = false;
-        started = true;
-
+    for (let i in uniqueScores) {
+      if (i < 10) {
+        let li = document.createElement("li");
+        li.textContent = uniqueScores[i];
+        personalBests.appendChild(li);
+      } else break;
     }
-    if (over) {
 
-        scoreList.push(score);
-        scoreList.sort(function compareNumbers(a, b) {
-            return a - b
-         });
-        scoreList.reverse();
-        uniqueScores = [...new Set(scoreList)];
-
-        personalBests.textContent = ""
-
-
-        for(let i in uniqueScores){
-            if (i<10){
-            let li = document.createElement("li")
-            li.textContent = uniqueScores[i];
-            personalBests.appendChild(li);
-            }else break;
-        }
-
-        clearInterval(gravityInterval)
-        started = false;
-    }
+    clearInterval(gravityInterval);
+    started = false;
+  }
 }
-
 
 function gravity() {
-    let birdStyle = window.getComputedStyle(bird);
-    let birdTop = parseInt(birdStyle.getPropertyValue("top"));
-    if (birdTop < 750 && birdTop > -50) {
-        bird.style.top = birdTop + 5 + "px";
-    }
+  let birdStyle = window.getComputedStyle(bird);
+  let birdTop = parseInt(birdStyle.getPropertyValue("top"));
+  if (birdTop < 750 && birdTop > -50) {
+    bird.style.top = birdTop + 5 + "px";
+  }
 }
 
-
 function jump() {
-    if (started) {
-        jumping = 1;
-        let jumpTime = 0;
+  if (started) {
+    let jumpTime = 0;
 
-        let jumpingInterval = setInterval(function () {
-            let birdStyle = window.getComputedStyle(bird);
-            let birdTop = parseInt(birdStyle.getPropertyValue("top"));
+    let jumpingInterval = setInterval(function () {
+      let birdStyle = window.getComputedStyle(bird);
+      let birdTop = parseInt(birdStyle.getPropertyValue("top"));
 
-            if (birdTop > 100) {
-                bird.style.top = parseInt(birdTop) - 7 + "px";
-            }
-            // jumpTime == 15 * 10 ms
-            if (jumpTime > 15 || over) {
-                clearInterval(jumpingInterval);
-                jumpTime = 0;
-                jumping = 0;
-            }
-            jumpTime++;
-        }, 10)
-    }
+      if (birdTop > 100) {
+        bird.style.top = parseInt(birdTop) - 7 + "px";
+      }
+      // jumpTime == 15 * 10 ms
+      if (jumpTime > 15 || over) {
+        clearInterval(jumpingInterval);
+        jumpTime = 0;
+      }
+      jumpTime++;
+    }, 10);
+  }
 }
 
 pipe.addEventListener("animationiteration", function () {
-    let random = Math.random();
-    let generatedTop = random * 400 + 100;
-    hole.style.top = generatedTop + "px";
-    topPipe.style.height = hole.style.top;
-    bottomPipe.style.top = parseInt(hole.style.top) + 200 + "px";
-    bottomPipe.style.height = 800 - parseInt(bottomPipe.style.top) +"px";
-    
-    score++;
-    scoreBox.textContent = "Score: " + score;
-})
+  let random = Math.random();
+  let generatedTop = random * 400 + 100;
+  hole.style.top = generatedTop + "px";
+  topPipe.style.height = hole.style.top;
+  bottomPipe.style.top = parseInt(hole.style.top) + 200 + "px";
+  bottomPipe.style.height = 800 - parseInt(bottomPipe.style.top) + "px";
 
+  score++;
+  scoreBox.textContent = "Score: " + score;
+});
 
 function gameOver() {
+  if (!over) {
+    let birdStyle = window.getComputedStyle(bird);
+    let birdTop = parseInt(birdStyle.getPropertyValue("top"));
+    let holeStyle = window.getComputedStyle(hole);
+    let holeTop = parseInt(holeStyle.getPropertyValue("top"));
+    let pipeStyle = window.getComputedStyle(pipe);
+    let pipeLeft = parseInt(pipeStyle.getPropertyValue("left"));
 
-    if (!over) {
+    if (
+      pipeLeft > 20 &&
+      pipeLeft < 130 &&
+      (birdTop <= holeTop || birdTop + 50 >= holeTop + 200)
+    ) {
+      pipe.style.animationPlayState = "paused";
+      over = true;
+      startGame();
+      let newline = "\r\n";
+      startMessage.textContent = "GAME OVER SCORE: " + score;
+      startBox.style.visibility = "visible";
 
-        let birdStyle = window.getComputedStyle(bird);
-        let birdTop = parseInt(birdStyle.getPropertyValue("top"));
-        let holeStyle = window.getComputedStyle(hole);
-        let holeTop = parseInt(holeStyle.getPropertyValue("top"));
-        let pipeStyle = window.getComputedStyle(pipe);
-        let pipeLeft = parseInt(pipeStyle.getPropertyValue("left"));
-
-
-        if (((pipeLeft > 20 && pipeLeft < 130) && ((birdTop <= holeTop) || (birdTop + 50 >= holeTop + 200)))) {
-
-            pipe.style.animationPlayState = "paused"
-            over = true;
-            startGame();
-            let newline = '\r\n'
-            startMessage.textContent= "GAME OVER SCORE: " + score;
-            startBox.style.visibility = "visible";
-
-            clearInterval(checkIfOver);
-        }
-
-        if (birdTop >= 750) {
-            pipe.style.animationPlayState = "paused";
-            over = true;
-            startGame();
-            startMessage.textContent= "GAME OVER SCORE: " + score;
-            startBox.style.visibility = "visible";
-            clearInterval(checkIfOver);
-
-        }
+      clearInterval(checkIfOver);
     }
+
+    if (birdTop >= 750) {
+      pipe.style.animationPlayState = "paused";
+      over = true;
+      startGame();
+      startMessage.textContent = "GAME OVER SCORE: " + score;
+      startBox.style.visibility = "visible";
+      clearInterval(checkIfOver);
+    }
+  }
 }
-
-
-
