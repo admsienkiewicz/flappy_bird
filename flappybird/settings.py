d9bec099e8a1e52a763c03971e6cff66ed6a45a9
admsienkiewicz <adm.sienkiewicz@gmail.com>
2021-11-16 21:32:50 +0100

    heroku deploy try22

--- flappybird/settings.py
+++ flappybird/settings.py
@@ -133,6 +133,7 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'game/static'),
 )
 
 # #  Add configuration for static files storage using whitenoise
